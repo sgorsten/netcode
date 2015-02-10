0d73f5c6b1d05f7e3050d497d04ee2b80fc13fb4
Sterling Orsten <sgorsten@gmail.com>
2015-02-10 02:30:16 -0800

    Add a simple, copycenter license to all source files.

--- src/Server.h
+++ src/Server.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2015 Sterling Orsten
+//   This software is provided 'as-is', without any express or implied
+// warranty. In no event will the author be held liable for any damages
+// arising from the use of this software. You are granted a perpetual, 
+// irrevocable, world-wide license to copy, modify, and redistribute
+// this software for any purpose, including commercial applications.
+
 #ifndef NETCODE_SERVER_H
 #define NETCODE_SERVER_H
 
